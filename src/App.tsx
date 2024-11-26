7e147878a0549a578bff682411ab749c5996f31f
Salman <salmanulfarisp9946@gmail.com>
2024-11-26 14:37:06 +0530

    react router and dependencies settled

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./rootLayout/RootLayout";
+import Home from "./page/Home";
+
+function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+}
+
+export default App;
