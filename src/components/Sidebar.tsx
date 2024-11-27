5b082a3eaabdbcfdfdc67ac77a9c0f42c025f30b
Salman <salmanulfarisp9946@gmail.com>
2024-11-27 09:29:44 +0530

    sidebar working

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from "react";
-
+import { useEffect, useState } from "react";
 interface Product {
-  categories: string;
+  category: string;
 }
 
 interface FetchResponse {
@@ -9,7 +8,7 @@ interface FetchResponse {
 }
 
 const Sidebar = () => {
-  const [categories, setcategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
   const [keyWords] = useState<string[]>([
     "apple",
     "watch",
@@ -24,14 +23,76 @@ const Sidebar = () => {
       try {
         const response = await fetch("https://dummyjson.com/products");
         const data: FetchResponse = await response.json();
-        console.log(data);
+        const uniqueCategories = Array.from(
+          new Set(data.products.map((product) => product.category))
+        );
+        setCategories(uniqueCategories);
       } catch (error) {
         console.error("error fetching product", error);
       }
     };
     fetchCategories();
   }, []);
-  return <div>Sidebar</div>;
+  return (
+    <div className="w-64 h-screen p-5">
+      <h1 className="mt-4 text-2xl font-bold mb-18">React Store</h1>
+
+      <section>
+        <input
+          className="w-full px-2 mt-3 border-2 rounded sm:mb-0"
+          type="text"
+          placeholder="Search Product"
+        />
+        <div className="flex items-center justify-between ">
+          <input
+            className="w-full px-5 py-3 mb-3 mr-2 border-2"
+            type="text"
+            placeholder="min"
+          />{" "}
+          <input
+            className="w-full px-5 py-3 mb-3 border-2"
+            type="text"
+            placeholder="max"
+          />
+        </div>
+
+        {/* categories Section  */}
+        <div className="mb-3 ">
+          <h2 className="mb-3 text-xl font-semibold">Categories</h2>
+        </div>
+        <section>
+          {categories.map((category, index) => (
+            <label className="block mb-2" key={index}>
+              <input
+                type="radio"
+                name="category"
+                value={category}
+                className="m-1 size-[16px]"
+              />
+              {category.toUpperCase()}
+            </label>
+          ))}
+        </section>
+
+        {/* Keywords Section  */}
+
+        <div className="mb-bottom">
+          <h2 className="mb-3 text-xl font-semibold ">Keywords</h2>
+          {keyWords.map((keyword, index) => (
+            <button
+              key={index}
+              className="block w-full px-2 py-2 mb-2 text-left border rounded hover:bg-gray-200"
+            >
+              {keyword.toUpperCase()}
+            </button>
+          ))}
+        </div>
+        <button className="w-full py-2 mt-5 mb-3 font-semibold text-white bg-black rounded">
+          Reset Filters
+        </button>
+      </section>
+    </div>
+  );
 };
 
 export default Sidebar;
